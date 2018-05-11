6eb405a2b3a4e654ca9fcd039a6a02859ed5c8a7
Alexander Melo <alexandermelox@gmail.com>
2018-05-11 14:00:12 -0400

    added bootstrap styling to the pages

--- app.js
+++ app.js
@@ -15,6 +15,18 @@ const campgrounds = [
   {
     name: 'Mountain Goat\'s Rest',
     image: 'https://images.unsplash.com/19/nomad.JPG?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjE2ODQ0fQ&s=da1f2d5da6e0fff9a6c4c4327af6771d&auto=format&fit=crop&w=1588&q=80'
+  },
+  {
+    name: 'Salmon Creek',
+    image: 'https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c85daa025ee04c951b6ac12fe3ba031a&auto=format&fit=crop&w=1950&q=80'
+  },
+  {
+    name: 'Granite Hill',
+    image: 'https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-0.3.5&s=73115e54fa3d099fcb2d92ccf12eee41&auto=format&fit=crop&w=1953&q=80'
+  }, 
+  {
+    name: 'Mountain Goat\'s Rest',
+    image: 'https://images.unsplash.com/19/nomad.JPG?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjE2ODQ0fQ&s=da1f2d5da6e0fff9a6c4c4327af6771d&auto=format&fit=crop&w=1588&q=80'
   }
 ];
 

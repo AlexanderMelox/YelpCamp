f9e013e75a457567b43ebbc8804c3c7ffbe36782
Alexander Melo <alexandermelox@gmail.com>
2018-05-12 21:10:52 -0400

    added mongoose and commented code

--- app.js
+++ app.js
@@ -1,38 +1,39 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+// Initializes all dependecies
+const express = require('express'),
+      app = express(),
+      bodyParser = require('body-parser'),
+      mongoose = require('mongoose');
 
+// Sets the port to listen to
 const port = 3000;
-const campgrounds = [
-  {
-    name: 'Salmon Creek',
-    image: 'https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c85daa025ee04c951b6ac12fe3ba031a&auto=format&fit=crop&w=1950&q=80'
-  },
-  {
-    name: 'Granite Hill',
-    image: 'https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-0.3.5&s=73115e54fa3d099fcb2d92ccf12eee41&auto=format&fit=crop&w=1953&q=80'
-  }, 
-  {
-    name: 'Mountain Goat\'s Rest',
-    image: 'https://images.unsplash.com/19/nomad.JPG?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjE2ODQ0fQ&s=da1f2d5da6e0fff9a6c4c4327af6771d&auto=format&fit=crop&w=1588&q=80'
-  },
-  {
-    name: 'Salmon Creek',
-    image: 'https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c85daa025ee04c951b6ac12fe3ba031a&auto=format&fit=crop&w=1950&q=80'
-  },
-  {
-    name: 'Granite Hill',
-    image: 'https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-0.3.5&s=73115e54fa3d099fcb2d92ccf12eee41&auto=format&fit=crop&w=1953&q=80'
-  }, 
-  {
-    name: 'Mountain Goat\'s Rest',
-    image: 'https://images.unsplash.com/19/nomad.JPG?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjE2ODQ0fQ&s=da1f2d5da6e0fff9a6c4c4327af6771d&auto=format&fit=crop&w=1588&q=80'
-  }
-];
 
+// Mongoose connects to mongoDB
+mongoose.connect('mongodb://localhost/yelp_camp');
+// Tells express to use body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
+// Sets the view engines to ejs
 app.set('view engine', 'ejs');
 
+// Schema Setup
+const campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
+
+// Creates a mongoose model
+const Campground = mongoose.model('Campground', campgroundSchema);
+
+// Inserts new campground to database
+Campground.create({
+    name: 'Granite Hill',
+    image: 'https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-0.3.5&s=73115e54fa3d099fcb2d92ccf12eee41&auto=format&fit=crop&w=1953&q=80'
+  }, (err, campground) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Newly created campground', campground);
+  } 
+});
 
 app.get('/', (req, res) => {
   res.render('landing');
@@ -46,7 +47,9 @@ app.get('/campgrounds', (req, res) => {
 app.post('/campgrounds', (req, res) => {
   // get data from form and add to campgrounds array
   const { name, image } = req.body;
+  // creates an object with the request body
   const newCampground = { name, image };
+  // pushes the newCampground object to campgrounds array
   campgrounds.push(newCampground);
   
   // redirect back to campgrounds page
@@ -55,4 +58,5 @@ app.post('/campgrounds', (req, res) => {
 
 app.get('/campgrounds/new', (req, res) => res.render('new'));
 
+// Tells express to listen on port and logs to the console
 app.listen(port, () => console.log('App is running on port ' + port));

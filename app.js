632cb19a0c3fdb4376185082d02bcffca0e327bf
Alexander Melo <alexandermelox@gmail.com>
2018-05-14 12:22:08 -0400

    Added description to campgrounds and campgrounds show page.

--- app.js
+++ app.js
@@ -17,23 +17,25 @@ app.set('view engine', 'ejs');
 // Schema Setup
 const campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 // Creates a mongoose model
 const Campground = mongoose.model('Campground', campgroundSchema);
 
 // Inserts new campground to database
-// Campground.create({
-//     name: 'Granite Hill',
-//     image: 'https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-0.3.5&s=73115e54fa3d099fcb2d92ccf12eee41&auto=format&fit=crop&w=1953&q=80'
-//   }, (err, campground) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log('Newly created campground', campground);
-//   } 
-// });
+Campground.create({
+    name: 'Granite Hill',
+    image: 'https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-0.3.5&s=73115e54fa3d099fcb2d92ccf12eee41&auto=format&fit=crop&w=1953&q=80',
+    description: 'This is a huge granite hill, no bathrooms, no water. Beautiful granite'
+  }, (err, campground) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Newly created campground', campground);
+  } 
+});
 
 app.get('/', (req, res) => {
   res.render('landing');
@@ -50,6 +52,7 @@ app.get('/campgrounds', (req, res) => {
   });
 });
 
+// CREATE - adds new campground to database
 app.post('/campgrounds', (req, res) => {
   // get data from form and add to campgrounds array
   const { name, image } = req.body;
@@ -66,7 +69,14 @@ app.post('/campgrounds', (req, res) => {
   });
 });
 
+// NEW - show form to create a new campground
 app.get('/campgrounds/new', (req, res) => res.render('new'));
+
+app.get('/camgrounds/:id', (req, res) => {
+  // finde the campground with provided id
+  // render show template with that campground
+  res.send('This will be the show page one day!');
+});
 
 // Tells express to listen on port and logs to the console
 app.listen(port, () => console.log('App is running on port ' + port));

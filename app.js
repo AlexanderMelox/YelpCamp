c7653eaecdade401f961dbfaac6c9d29d40b84ff
Alexander Melo <alexandermelox@gmail.com>
2018-05-14 19:15:32 -0400

    changed some typos and rendered show template

--- app.js
+++ app.js
@@ -25,17 +25,17 @@ const campgroundSchema = new mongoose.Schema({
 const Campground = mongoose.model('Campground', campgroundSchema);
 
 // Inserts new campground to database
-Campground.create({
-    name: 'Granite Hill',
-    image: 'https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-0.3.5&s=73115e54fa3d099fcb2d92ccf12eee41&auto=format&fit=crop&w=1953&q=80',
-    description: 'This is a huge granite hill, no bathrooms, no water. Beautiful granite'
-  }, (err, campground) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('Newly created campground', campground);
-  } 
-});
+// Campground.create({
+//     name: 'Granite Hill',
+//     image: 'https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-0.3.5&s=73115e54fa3d099fcb2d92ccf12eee41&auto=format&fit=crop&w=1953&q=80',
+//     description: 'This is a huge granite hill, no bathrooms, no water. Beautiful granite'
+//   }, (err, campground) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log('Newly created campground', campground);
+//   } 
+// });
 
 app.get('/', (req, res) => {
   res.render('landing');
@@ -72,10 +72,11 @@ app.post('/campgrounds', (req, res) => {
 // NEW - show form to create a new campground
 app.get('/campgrounds/new', (req, res) => res.render('new'));
 
-app.get('/camgrounds/:id', (req, res) => {
-  // finde the campground with provided id
+// SHOWS - shows more info about one campground
+app.get('/campgrounds/:id', (req, res) => {
+  // find the campground with provided id
   // render show template with that campground
-  res.send('This will be the show page one day!');
+  res.render('show');
 });
 
 // Tells express to listen on port and logs to the console
